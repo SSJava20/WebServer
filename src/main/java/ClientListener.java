dce2c660edbc0efeb620925edd7773f872e860b3
Vladislav <root@stvad.org>
2012-03-12 21:14:02 +0200

    some added

--- src/main/java/ClientListener.java
+++ src/main/java/ClientListener.java
@@ -1,3 +1,7 @@
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
 /**
  * Created by IntelliJ IDEA.
  * User: stvad
@@ -5,6 +9,16 @@
  * Time: 21:02
  * To change this template use File | Settings | File Templates.
  */
-public class ClientListener
+public class ClientListener implements Runnable
 {
+    protected Socket socket;
+    protected InputStream inputStream;
+    protected OutputStream outputStream;
+
+
+    @Override
+    public void run()
+    {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
 }
