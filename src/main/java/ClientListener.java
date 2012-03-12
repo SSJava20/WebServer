fcb99d3e0cdf58d7dda7f4d705e5a9cc7b5a986f
Roman Kostyrko <nubaseg@gmail.com>
2012-03-12 22:09:08 +0300

    correct format

--- src/main/java/ClientListener.java
+++ src/main/java/ClientListener.java
@@ -1,10 +1,6 @@
 /**
- * Created by IntelliJ IDEA.
- * User: stvad
- * Date: 12.03.12
- * Time: 21:02
- * To change this template use File | Settings | File Templates.
+ * Created by IntelliJ IDEA. User: stvad Date: 12.03.12 Time: 21:02 To change
+ * this template use File | Settings | File Templates.
  */
-public class ClientListener
-{
+public class ClientListener {
 }
--- src/main/java/WebServer.java
+++ src/main/java/WebServer.java
@@ -1,10 +1,6 @@
 /**
- * Created by IntelliJ IDEA.
- * User: stvad
- * Date: 12.03.12
- * Time: 20:55
- * To change this template use File | Settings | File Templates.
+ * Created by IntelliJ IDEA. User: stvad Date: 12.03.12 Time: 20:55 To change
+ * this template use File | Settings | File Templates.
  */
-public class WebServer
-{
+public class WebServer {
 }
