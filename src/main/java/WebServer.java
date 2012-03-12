9742e5db4c2ac7baafb19660b2dd5fa0698834b5
Vladislav <root@stvad.org>
2012-03-12 20:56:55 +0200

    olol

---
+++ src/main/java/WebServer.java
@@ -0,0 +1,10 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: stvad
+ * Date: 12.03.12
+ * Time: 20:55
+ * To change this template use File | Settings | File Templates.
+ */
+public class WebServer
+{
+}
