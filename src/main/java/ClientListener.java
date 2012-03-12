4dcd66b3ea55693bdf6cca974e47260f0647bb9c
Vladislav <root@stvad.org>
2012-03-12 21:03:30 +0200

    add file test commit

---
+++ src/main/java/ClientListener.java
@@ -0,0 +1,10 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: stvad
+ * Date: 12.03.12
+ * Time: 21:02
+ * To change this template use File | Settings | File Templates.
+ */
+public class ClientListener
+{
+}
